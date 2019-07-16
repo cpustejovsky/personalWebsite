7d6c8e16b8581d273624d96610d8c5217550eefd
cpustejovsky <charles.pustejovsky@gmail.com>
2019-07-16 19:46:49 -0400

    add bootstrap files added fontawesome cdn and icons to navigation added in navigation set up homepage added dynamic date to footer small styling added a test.html until I figure out how to make Prettier play nice with my EJS partials

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ const express = require("express");
 const app = express();
 const favicon = require("serve-favicon");
 const port = (process.env.PORT = 3000);
+const moment = require("moment");
+
 app.set("view engine", "ejs");
 app.use("/assets", express.static(__dirname + "/public"));
 app.use(favicon(__dirname + "/public/images/favicon.ico"));
+app.use("/assets", express.static(__dirname + "/public"));
+
+app.use(function(req, res, next) {
+  res.locals.today = moment().format("DD MMMM YYYY");
+  next();
+});
+
+//ROUTES
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.use("/assets", express.static(__dirname + "/public"));
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("reading-list", (req, res) => {
+  res.render("reading-list");
+});
+app.get("projects", (req, res) => {
+  res.render("projects");
+});
+app.get("/life-together-calculator", (req, res) => {
+  res.render("life-together-calculator");
+});
 
 app.listen(port, (req, res) => {
   console.log(`listening on ${port}`);
