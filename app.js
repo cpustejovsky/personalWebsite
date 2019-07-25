ae3053c523663bc4bc995cd6b83e28bb9467085a
cpustejovsky <charles.pustejovsky@gmail.com>
2019-07-25 10:10:48 -0400

    adding test routes for a nodeJS exercise I'm working on so I can debug it better

--- app.js
+++ app.js
@@ -28,6 +28,24 @@ app.get("/life-together-calculator", (req, res) => {
   res.render("life-together-calculator");
 });
 
+app.get("/test-route1", (req, res) => {
+  res.send(
+    "On August 12th, 2018, I would have been ordained in the Lutheran Church — Missouri Synod if I had stayed on that path. But four and a half years ago, I chose to not go to seminary."
+  );
+});
+
+app.get("/test-route2", (req, res) => {
+  res.send(
+    "Basically, it’s the distinction between what you love and what you love to do. I’ll use myself as an example."
+  );
+});
+
+app.get("/test-route3", (req, res) => {
+  res.send(
+    "I can spend hours coding or learning about development without getting bored. My frustrations increase my resolve and determination to find a solution. My wife will tell me, “come to bed” and I’ll respond “just a few more minutes.” The only thing that has ever done that for me is… video games. Programming is the only thing I’ve found that competes with video games for my attention."
+  );
+});
+
 app.listen(port, process.env.IP, (req, res) => {
   console.log(`listening on ${port}`);
 });
