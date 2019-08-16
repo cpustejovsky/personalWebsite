c20e2504afa73c6a8687ded9e943c4c84c7e543a
cpustejovsky <charles.pustejovsky@gmail.com>
2019-08-15 20:38:03 -0400

    some styling changes to life together calculator

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const favicon = require("serve-favicon");
 const moment = require("moment");
 const bodyParser = require("body-parser");
-const indexRoutes = require('./routes/index');
-const lifeTogetherRoutes = require('./routes/lifeTogether');
+const indexRoutes = require("./routes/index");
+const lifeTogetherRoutes = require("./routes/lifeTogether");
 let port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
@@ -13,16 +13,12 @@ app.use(favicon(__dirname + "/public/images/favicon.ico"));
 app.use("/assets", express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.locals.today = moment().format("DD MMMM YYYY");
   next();
 });
 app.use("/life-together-calculator", lifeTogetherRoutes);
 app.use("/", indexRoutes);
-app.get("/*", (req, res) => {
-  console.log("404");
-  res.render("index");
-});
 
 app
   .listen(port, () => {
