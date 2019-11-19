e43e108a9e04f192a43b1b8d4f9e930e758c0ec7
cpustejovsky <charles.pustejovsky@gmail.com>
2019-11-18 20:44:46 -0500

    fixed naming error

--- middleware/dynoWaker.js
+++ middleware/dynoWaker.js
@@ -1,13 +1,13 @@
 const fetch = require("node-fetch");
 const moment = require("moment");
 
-const checkTime = () => {
+const dynoWaker = () => {
     if (sleep) {
         let now = moment();
         if (now.hours() === 6 && now.minutes() === 0 && now.format("A") === "AM") {
             fetch("https://cpustejovsky-estuary.herokuapp.com/")
         } else {
-            setTimeout(checkTime, 1000 * 30);
+            setTimeout(dynoWaker, 1000 * 30);
         }
     }
 }
