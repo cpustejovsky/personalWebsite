c012b0ccd2b38e1e38a68972fac4cb8126c7459a
cpustejovsky <charles.pustejovsky@gmail.com>
2019-11-18 20:47:32 -0500

    removed unused conditional left over from previous project

--- middleware/dynoWaker.js
+++ middleware/dynoWaker.js
@@ -2,13 +2,11 @@ const fetch = require("node-fetch");
 const moment = require("moment");
 
 const dynoWaker = () => {
-    if (sleep) {
-        let now = moment();
-        if (now.hours() === 6 && now.minutes() === 0 && now.format("A") === "AM") {
-            fetch("https://cpustejovsky-estuary.herokuapp.com/")
-        } else {
-            setTimeout(dynoWaker, 1000 * 30);
-        }
+    let now = moment();
+    if (now.hours() === 6 && now.minutes() === 0 && now.format("A") === "AM") {
+        fetch("https://cpustejovsky-estuary.herokuapp.com/")
+    } else {
+        setTimeout(dynoWaker, 1000 * 30);
     }
 }
 
