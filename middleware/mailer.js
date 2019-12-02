480a398d5646fae80e5feb7318d7e94b5ca8fcb6
cpustejovsky <charles.pustejovsky@gmail.com>
2019-12-01 20:51:19 -0500

    updated mailer.js

--- middleware/mailer.js
+++ middleware/mailer.js
@@ -3,16 +3,16 @@ const bot = require("cpustejovsky-twitter-bot");
 const nodemailer = require("nodemailer");
 //TODO: Try and move these functions in bot.js
 //TODO: Add in cron functionality and run at same time as my notes and the dynoWaker
-async function likeAndRetweet(tweetData) {
-    let completeTweets = [];
-    for (let i = 0; i < tweetData.tweets.length; i++) {
-        let liked = await bot.like(tweetData.tweets[i]);
-        let retweeted = await bot.retweet(liked);
-        completeTweets.push(retweeted);
-    }
-    tweetData.tweets = completeTweets;
-    return tweetData;
-}
+// async function likeAndRetweet(tweetData) {
+//     let completeTweets = [];
+//     for (let i = 0; i < tweetData.tweets.length; i++) {
+//         let liked = await bot.like(tweetData.tweets[i]);
+//         let retweeted = await bot.retweet(liked);
+//         completeTweets.push(retweeted);
+//     }
+//     tweetData.tweets = completeTweets;
+//     return tweetData;
+// }
 
 async function emailTweets(emailAddress, data) {
     let transporter = nodemailer.createTransport({
@@ -40,10 +40,11 @@ async function emailTweets(emailAddress, data) {
     console.log("Message sent: %s", info.messageId);
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 };
-
-bot.getTwitterData("CCPustejovsky", 2)
+//TODO: set this up as a cronjob for wife and archbishop
+bot.getTwitterData("FluffyHookers", 10)
     .then(async (data) => {
-        let likedAndRetweeted = await likeAndRetweet(data);
-        console.log(likedAndRetweeted)
-        // emailTweets("cpustejovsky@bitpay.com", likedAndRetweeted);
+        let likedTweets = await bot.like(data);
+        let retweetedTweets = await bot.retweet(likedTweets);
+        console.log(retweetedTweets);
+        await emailTweets("charles.pustejovsky@gmail.com", retweetedTweets);
     });
