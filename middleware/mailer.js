211540c9c20f2daa0dd7d7507f78297ad4ab15c1
cpustejovsky <charles.pustejovsky@gmail.com>
2019-12-08 13:07:21 -0500

    trying to remove .idea files from tracking; also updated twitteremail to only like and email me new tweets

--- middleware/mailer.js
+++ middleware/mailer.js
@@ -38,9 +38,7 @@ function tweetEmailUpdate(screenName, count, email) {
     bot.getTwitterData(screenName, count)
         .then(async (data) => {
             let likedTweets = await bot.like(data);
-            let retweetedTweets = await bot.retweet(likedTweets);
-            console.log(retweetedTweets);
-            await emailTweets(email, retweetedTweets);
+            await emailTweets(email, likedTweets);
         });
 };
 
